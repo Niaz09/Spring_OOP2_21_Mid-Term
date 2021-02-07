93dd1cd1792e27767d1578ebaded68580812c054
Niaz Rahman Bhuiyan <78007945+Niaz09@users.noreply.github.com>
2021-02-07 11:52:05 +0600

    Add files via upload

---
+++ Number_01.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _18_38770_3__Lab_03_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			int i;
+			Console.WriteLine("Even numbers between 1 and 100:\n");
+			for (i = 1; i <= 100; i++)
+			{
+				if (i % 2 == 0)
+				{
+					Console.WriteLine(i + " ");
+				}
+			}
+		}
+    }
+}
